3435345834900ce2fcc8e452317cce3f5ccf643a
Shreyas Balapure <shreyashbalapure@gmail.com>
2025-07-30 21:33:17 +0530

    Added doctor service

---
+++ backend/doctor-service/doctor-service/src/main/java/com/hms/doctorservice/dto/DoctorDTO.java
@@ -0,0 +1,12 @@
+package com.hms.doctorservice.dto;
+
+import java.util.List;
+
+public class DoctorDTO {
+	public String name;
+	public String email;
+	public String contact;
+	public String speciality;
+	public String qualification;
+	public List<String> availability;
+}
