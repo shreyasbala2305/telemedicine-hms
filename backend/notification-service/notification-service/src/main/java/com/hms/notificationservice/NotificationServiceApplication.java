1519f86613c0704a7a820b4ed7ca66498b6b54a4
Shreyas Balapure <shreyashbalapure@gmail.com>
2025-10-15 18:34:21 +0530

    Fix Eureka client registration issue for all services

--- backend/notification-service/notification-service/src/main/java/com/hms/notificationservice/NotificationServiceApplication.java
+++ backend/notification-service/notification-service/src/main/java/com/hms/notificationservice/NotificationServiceApplication.java
@@ -2,9 +2,11 @@ package com.hms.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableFeignClients
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
--- backend/patient-service/patient-service/src/main/java/com/hms/patientservice/PatientServiceApplication.java
+++ backend/patient-service/patient-service/src/main/java/com/hms/patientservice/PatientServiceApplication.java
@@ -2,10 +2,12 @@ package com.hms.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
